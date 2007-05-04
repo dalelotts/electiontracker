8911afca68ebd360ba4bb4b6cfd656a0cd02e620
UWEC\degenst <UWEC\degenst@a75ae3dc-1c41-0410-92f9-01bff1c2377c>
2007-05-04 02:17:57 +0000

    This and Last revision include a reworking of the way reports are handled (inherit frmAbstractForm).  Also, proofing sheet printout is done; untested;
    Modified:
    Added new abstract report.
    Deleted (?) Some designer files for forms that no longer are designable (ie they inherit from the abstract report form).

--- ET/ui/frmContestVoteSumry.cs
+++ ET/ui/frmContestVoteSumry.cs
@@ -1,18 +1,10 @@
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Text;
-using System.Windows.Forms;
 
 namespace edu.uwec.cs.cs355.group4.et.ui
 {
-    public partial class frmContestVoteSumry : Form
+    class frmContestVoteSumry : frmAbstractReport 
     {
-        public frmContestVoteSumry()
-        {
-            InitializeComponent();
-        }
     }
 }
