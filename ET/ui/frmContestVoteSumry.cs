03b3c4df95ba3a1488a23528aa3db75ccbe74442
UWEC\degenst <UWEC\degenst@a75ae3dc-1c41-0410-92f9-01bff1c2377c>
2007-05-04 02:36:09 +0000

    Consider revisions 40,41,and 42 to all go by 41's description.  I was having issues with file deletion and creation in the repository, but I've figured it out.  Sorry to clutter our revision log.

--- ET/ui/frmContestVoteSumry.cs
+++ ET/ui/frmContestVoteSumry.cs
@@ -1,10 +1,35 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Text;
+using System.Windows.Forms;
+using edu.uwec.cs.cs355.group4.et.db;
+using System.Drawing.Printing;
+using edu.uwec.cs.cs355.group4.et.core;
+using edu.uwec.cs.cs355.group4.et.ui.util;
 
 namespace edu.uwec.cs.cs355.group4.et.ui
 {
     class frmContestVoteSumry : frmAbstractReport 
     {
+        frmContestVoteSumry(ElectionDAO electionDAO)
+            : base(electionDAO) { }
+
+        protected override string GetTitle()
+        {
+            throw new Exception("The method or operation is not implemented.");
+        }
+
+        protected override IList<Election> GetElections()
+        {
+            throw new Exception("The method or operation is not implemented.");
+        }
+
+        protected override void CreateReport(Election elc)
+        {
+            throw new Exception("The method or operation is not implemented.");
+        }
     }
 }
--- ET/ui/frmCountyContactForm.cs
+++ ET/ui/frmCountyContactForm.cs
@@ -5,14 +5,33 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using edu.uwec.cs.cs355.group4.et.db;
+using System.Drawing.Printing;
+using edu.uwec.cs.cs355.group4.et.core;
+using edu.uwec.cs.cs355.group4.et.ui.util;
 
 namespace edu.uwec.cs.cs355.group4.et.ui
 {
-    public partial class frmCountyContactForm : Form
+    class frmCountyContactForm : frmAbstractReport 
     {
-        public frmCountyContactForm()
+        frmCountyContactForm(ElectionDAO electionDAO)
+            : base(electionDAO)
         {
-            InitializeComponent();
+        }
+
+        protected override string GetTitle()
+        {
+            throw new Exception("The method or operation is not implemented.");
+        }
+
+        protected override IList<Election> GetElections()
+        {
+            throw new Exception("The method or operation is not implemented.");
+        }
+
+        protected override void CreateReport(Election elc)
+        {
+            throw new Exception("The method or operation is not implemented.");
         }
     }
 }
