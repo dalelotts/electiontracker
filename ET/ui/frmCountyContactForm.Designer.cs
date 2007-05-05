9e19918b18a9224ae1dc604e2064d094d0d5e3ff
UWEC\degenst <UWEC\degenst@a75ae3dc-1c41-0410-92f9-01bff1c2377c>
2007-05-05 05:51:09 +0000

    Forgot to add files again.  That's what I get for doing it at 1 am...

---
+++ ET/ui/frmCountyContactForm.Designer.cs
@@ -0,0 +1,98 @@
+namespace edu.uwec.cs.cs355.group4.et.ui
+{
+    partial class frmCountyContactForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ppcViewer = new System.Windows.Forms.PrintPreviewControl();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.btnUp = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ppcViewer
+            // 
+            this.ppcViewer.Location = new System.Drawing.Point(12, 12);
+            this.ppcViewer.Name = "ppcViewer";
+            this.ppcViewer.Size = new System.Drawing.Size(524, 440);
+            this.ppcViewer.TabIndex = 0;
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(461, 458);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(75, 23);
+            this.btnPrint.TabIndex = 1;
+            this.btnPrint.Text = "&Print";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // btnDown
+            // 
+            this.btnDown.Location = new System.Drawing.Point(537, 244);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(20, 23);
+            this.btnDown.TabIndex = 12;
+            this.btnDown.Text = "v";
+            this.btnDown.UseVisualStyleBackColor = true;
+            this.btnDown.Click += new System.EventHandler(this.btnDown_Click);
+            // 
+            // btnUp
+            // 
+            this.btnUp.Location = new System.Drawing.Point(537, 215);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(21, 23);
+            this.btnUp.TabIndex = 11;
+            this.btnUp.Text = "^";
+            this.btnUp.UseVisualStyleBackColor = true;
+            this.btnUp.Click += new System.EventHandler(this.btnUp_Click);
+            // 
+            // frmCountyContactForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 487);
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.btnUp);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.ppcViewer);
+            this.Name = "frmCountyContactForm";
+            this.Text = "County Contact Listing";
+            this.Resize += new System.EventHandler(this.frmCountyContactForm_Resize);
+            this.Load += new System.EventHandler(this.frmCountyContactForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PrintPreviewControl ppcViewer;
+        private System.Windows.Forms.Button btnPrint;
+        protected System.Windows.Forms.Button btnDown;
+        protected System.Windows.Forms.Button btnUp;
+    }
+}
