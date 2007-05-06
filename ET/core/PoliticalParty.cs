9ad8b9436dbee6016dfc75b66cab1e04995a45e8
uwec\lottsdd <uwec\lottsdd@a75ae3dc-1c41-0410-92f9-01bff1c2377c>
2007-05-06 23:39:37 +0000

    Minor code cleanup

--- ET/core/PoliticalParty.cs
+++ ET/core/PoliticalParty.cs
@@ -3,14 +3,18 @@ using edu.uwec.cs.cs355.group4.et.db;
 
 namespace edu.uwec.cs.cs355.group4.et.core {
     public class PoliticalParty {
-        private long    id;
-        private string  name;
-        private string  abbreviation;
+        private long id;
+        private string name;
+        private string abbreviation;
         private IList<Candidate> candidates;
-        private bool    isActive = true;
+        private bool isActive = true;
 
+<<<<<<< .mine
+        public virtual IList<Candidate> Candidates {
+=======
         #region Properties
         public virtual IList<Candidate>  Candidates {
+>>>>>>> .r61
             get { return candidates; }
             set { candidates = value; }
         }
