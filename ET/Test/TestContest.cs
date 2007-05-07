4178eb8a2f99d494cbb22b774b650910046b7598
UWEC\ROGERSBS <UWEC\ROGERSBS@a75ae3dc-1c41-0410-92f9-01bff1c2377c>
2007-05-07 05:15:18 +0000

    TestContest

--- ET/Test/TestContest.cs
+++ ET/Test/TestContest.cs
@@ -8,7 +8,6 @@ namespace edu.uwec.cs.cs355.group4.et.core {
         [SetUp()]
         public void SetUp() {
             _unitUnderTest = new Contest();
-            _unitUnderTest.Name = "TestContest";
         }
 
         [TearDown()]
@@ -19,15 +18,43 @@ namespace edu.uwec.cs.cs355.group4.et.core {
         [Test()]
         public void TestToString() {
             string expectedString = "TestContest";
+            _unitUnderTest.Name = expectedString;
             string resultString = _unitUnderTest.ToString();
             Assert.AreEqual(expectedString, resultString, "ToString method returned unexpected result.");
         }
 
         [Test()]
-        public void TestEquals() {
-            object obj = null;
-            bool expectedBoolean = false;
-            bool resultBoolean = _unitUnderTest.Equals(obj);
+        public void TestEqualsReflexive() {
+            _unitUnderTest.ID = 1;
+            bool expectedBoolean = true;
+            bool resultBoolean = _unitUnderTest.Equals(_unitUnderTest);
+            Assert.AreEqual(expectedBoolean, resultBoolean, "Equals method returned unexpected result.");
+        }
+
+        [Test()]
+        public void TestEqualsSymmetric()
+        {
+            _unitUnderTest.ID = 1;
+            Contest c2 = new Contest();
+            c2.ID = 1;
+            bool expectedBoolean = true;
+            bool resultBoolean = _unitUnderTest.Equals(c2);
+            Assert.AreEqual(expectedBoolean, resultBoolean, "Equals method returned unexpected result.");
+        }
+
+        [Test()]
+        public void TestEqualsTransitive()
+        {
+            _unitUnderTest.ID = 1;
+            Contest c2 = new Contest();
+            c2.ID = 1;
+            Contest c3 = new Contest();
+            c3.ID = 1;
+            bool expectedBoolean = true;
+            bool ab = _unitUnderTest.Equals(c2);
+            bool bc = c2.Equals(c3);
+            bool ac = _unitUnderTest.Equals(c3);
+            bool resultBoolean = (ab && bc && ac);
             Assert.AreEqual(expectedBoolean, resultBoolean, "Equals method returned unexpected result.");
         }
     }
