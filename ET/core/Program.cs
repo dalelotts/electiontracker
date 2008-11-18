0c88f08531db4ea34417b3ba2d64ad11f77ef0f3
spinarkm <spinarkm@a75ae3dc-1c41-0410-92f9-01bff1c2377c>
2008-11-18 21:13:37 +0000

    Issue 28: Automatic Backup Added code to execute mysqldump to perform a backup at most once a day.

--- ET/core/Program.cs
+++ ET/core/Program.cs
@@ -17,6 +17,8 @@
  *  along with this program.  If not, see http://www.gnu.org/licenses/
  **/
 using System;
+using System.IO;
+using System.Xml;
 using System.Windows.Forms;
 using Common.Logging;
 using KnightRider.ElectionTracker.ui;
@@ -27,8 +29,62 @@ namespace KnightRider.ElectionTracker.core {
     internal static class Program {
         private static readonly ILog LOG = LogManager.GetLogger(typeof (Program));
 
+        private static String getBackupArguments()
+        {
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load("spring.xml");
+            XmlNode provider = xDoc.GetElementsByTagName("db:provider").Item(0);
+            XmlAttributeCollection attributes = provider.Attributes;
+            String connectionStr = attributes.GetNamedItem("connectionString").Value;
+
+            //parse: server=foo;database=bar;uid=baz;pwd=quux;
+            String[] connectionInfo = connectionStr.Split(new char[] { ';' });
+
+            String database = "";
+            String uid = "";
+            String pwd = "";
+
+            foreach (String item in connectionInfo)
+            {
+                if (item.StartsWith("database="))
+                {
+                    database = item.Substring(9);
+                }
+                else if (item.StartsWith("uid="))
+                {
+                    uid = item.Substring(4);
+                }
+                else if (item.StartsWith("pwd="))
+                {
+                    pwd = item.Substring(4);
+                }
+            }
+
+            return "--skip-opt -Q -u " + uid + " " + database + " -p " + pwd;
+        }
+
         [STAThread]
         public static void Main(string[] args) {
+            try
+            {
+                Directory.CreateDirectory("backups");
+                String filename = @"backups\backup" + DateTime.Now.ToString("yyyy-MM-dd") + ".sql";
+
+                if (!File.Exists(filename))
+                {
+                    System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                    proc.EnableRaisingEvents = false;
+                    proc.StartInfo.FileName = "mysqldump";
+                    proc.StartInfo.Arguments = getBackupArguments() + " > " + filename;
+                    proc.Start();
+                    proc.WaitForExit();
+                }
+            }
+            catch (Exception ex)
+            {
+                LOG.Error(ex.Message, ex);
+            }
+
             try {
                 Application.EnableVisualStyles();
                 IApplicationContext ctx = ContextRegistry.GetContext("spring.root");
